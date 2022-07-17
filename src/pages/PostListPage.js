2ab13020a487ac2963cd6b8bf5ba7d0586fa4464
yunrap <dbsdpwl7@gmail.com>
2022-07-17 18:14:23 +0900

    *new componet 생성

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,9 @@
+import Button from "../components/common/Button";
+
+export default function PostListPage() {
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
+}
---
+++ src/pages/WritePage.js
@@ -0,0 +1,3 @@
+export default function WritePage() {
+  return <div>글쓰기</div>;
+}
